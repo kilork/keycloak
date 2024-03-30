7bf058a72257509ab520d39d3783332b16035769
Alexander Korolev <alexander.korolev.germany@gmail.com>
2024-03-30 12:08:17 +0100

    require version 1.65 of Rust

--- src/lib.rs
+++ src/lib.rs
@@ -19,7 +19,7 @@ Default flags: `tags-all`.
 
 ## Usage
 
-Requires Rust version >= `1.63.0`.
+Requires Rust version >= `1.65.0`.
 
 Add dependency to Cargo.toml:
 
--- templates/README.md
+++ templates/README.md
@@ -18,7 +18,7 @@ Default flags: `tags-all`.
 
 ## Usage
 
-Requires Rust version >= `1.63.0`.
+Requires Rust version >= `1.65.0`.
 
 Add dependency to Cargo.toml:
 
