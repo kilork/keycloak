a29f7412e406944b62aac1fcfe5dda35fb9c6ed0
Alexander Korolev <alexander.korolev.germany@gmail.com>
2024-04-22 20:37:11 +0200

    Keycloak Admin REST API v24.0.300

--- src/lib.rs
+++ src/lib.rs
@@ -7,7 +7,7 @@ Dual-licensed under `MIT` or the [UNLICENSE](http://unlicense.org/).
 
 ## Features
 
-Implements [Keycloak Admin REST API version 24.0.2](https://www.keycloak.org/docs-api/24.0.2/rest-api/index.html).
+Implements [Keycloak Admin REST API version 24.0.3](https://www.keycloak.org/docs-api/24.0.3/rest-api/index.html).
 
 ### Feature flags
 
