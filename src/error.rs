1b8c076f62105d5c21eb537613ad293b82a68441
Tim <Tockra@users.noreply.github.com>
2025-03-11 15:10:40 +0100

    - add crate thiserror and removed manualy implementations

--- src/error.rs
+++ src/error.rs
@@ -1,38 +1,17 @@
-use std::{borrow::Cow, error::Error, fmt::Display};
-
 use serde::{Deserialize, Serialize};
+use thiserror::Error as ThisError;
 
-#[derive(Debug)]
+#[derive(ThisError, Debug)]
 pub enum KeycloakError {
-    ReqwestFailure(reqwest::Error),
+    #[error("keycloak error (network): {0}")]
+    ReqwestFailure(#[from] reqwest::Error),
+
+    #[error("keycloak error (rest): {status} {body:?} {text}")]
     HttpFailure {
         status: u16,
         body: Option<KeycloakHttpError>,
         text: String,
     },
-}
-
-impl From<reqwest::Error> for KeycloakError {
-    fn from(value: reqwest::Error) -> Self {
-        KeycloakError::ReqwestFailure(value)
-    }
-}
-
-impl Error for KeycloakError {}
-
-impl Display for KeycloakError {
-    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
-        match self {
-            KeycloakError::ReqwestFailure(e) => write!(f, "keycloak error (network): {e}"),
-            KeycloakError::HttpFailure { status, body, text } => write!(
-                f,
-                "keycloak error (rest): {status} {}",
-                body.as_ref()
-                    .and_then(|e| e.message())
-                    .unwrap_or_else(|| Cow::from(text))
-            ),
-        }
-    }
 }
 
 #[derive(Debug, Clone, Deserialize, Serialize)]
@@ -41,24 +20,4 @@ pub struct KeycloakHttpError {
     pub error_description: Option<String>,
     #[serde(rename = "errorMessage")]
     pub error_message: Option<String>,
-}
-
-impl KeycloakHttpError {
-    pub fn message(&self) -> Option<Cow<'_, str>> {
-        self.error_message
-            .as_deref()
-            .map(Cow::from)
-            .or_else(|| {
-                self.error
-                    .as_deref()
-                    .map(|error| {
-                        format!(
-                            "{} [{error}]",
-                            self.error_description.as_deref().unwrap_or("null")
-                        )
-                    })
-                    .map(Cow::from)
-            })
-            .or_else(|| self.error_description.as_deref().map(Cow::from))
-    }
 }
