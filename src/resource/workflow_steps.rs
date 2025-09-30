c8201a7a27240471628da44a84f21e72af0a24e4
Alexander Korolev <alexander.korolev.germany@gmail.com>
2025-09-30 23:31:24 +0200

    Keycloak Admin REST API v26.4.0 (#177)

---
+++ src/resource/workflow_steps.rs
@@ -0,0 +1,5 @@
+use super::*;
+
+impl<'a, TS: KeycloakTokenSupplier> KeycloakRealmAdmin<'a, TS> {
+    // <h4>Workflow Steps</h4>
+}
---
+++ src/rest/generated_rest/workflow_steps.rs
@@ -0,0 +1,7 @@
+use super::*;
+
+impl<TS: KeycloakTokenSupplier> KeycloakAdmin<TS> {
+    // <h4>Workflow Steps</h4>
+}
+// not all paths processed
+// left 247
